a5d3aa7f17933e251382aedc3ba305657b3e58c1
Storm Harris-Studdart <103412904+StormHS@users.noreply.github.com>
2023-07-17 14:57:26 +1200

    set up app and buttons

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,17 +2,31 @@
 
 function App() {
   //   const { data } = useFruits()
+  const linkedUrlLinkedin = `https://www.linkedin.com/in/storm-harris-studdart-803930208/`
 
   return (
     <>
       <div className="app">
-        <h1>Storm Harris-Studdart</h1>
-        <img
-          className="storm-img"
-          src="../images/storm.jpg"
-          alt="Image of Storm"
-        />
-
+        <div className="wrap-container">
+          <img
+            className="storm-img"
+            src="../client/Images/storm.jpg"
+            alt="Image of Storm"
+          />
+          <div className="head-cv">
+            <h1>Storm Harris-Studdart</h1>
+            <button className="buttons">CV</button>
+          </div>
+        </div>
+        <button>
+          <a href={linkedUrlLinkedin} className="buttons">
+            LinkedIn
+          </a>
+        </button>{' '}
+        <button className="buttons">Github</button>
+        <h2>Current Projects</h2>
+        <button className="buttons">Apprendre Le Français</button>
+        <button className="buttons">To Do To Day</button>
         {/* <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul> */}
       </div>
     </>
